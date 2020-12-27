27cf91aba4e1e6991b4f9c28ebb9fcd02005cfb1
Jin Kim <jinkim1195@gmail.com>
2020-12-27 12:34:49 -0800

    added method-override since forms can only do get/post

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { v4: uuid } = require('uuid');
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded( { extended: true } ));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -65,7 +67,16 @@ app.patch('/comments/:id', (req,res) => {
     res.redirect('/comments');
 });
 
+// GET to edit a comment for user
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find( c => c.id === id);
+
+    res.render('comments/edit', { comment });
+});
+
 // DELETE - destory a comment
+// app.delete('/comments/:')
 
 
 app.listen(3000, ()=>{
